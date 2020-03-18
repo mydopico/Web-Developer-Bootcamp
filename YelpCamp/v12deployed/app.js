3cd734b9e06c0ab32b29cf342f4ee6e3beb3c473
mydopico <mydopico@gmail.com>
2020-03-18 18:00:58 +0000

    update dburl

--- YelpCamp/v12deployed/app.js
+++ YelpCamp/v12deployed/app.js
@@ -17,7 +17,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useUnifiedTopology: true, useNewUrlParser: true});
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12";
+mongoose.connect(url);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
